5705d43cd15d45951c397d617067f117a9608064
rexxmagtar <rexx.magtar@gmail.com>
2020-05-30 02:40:23 +0300

    Ui tests added

--- task2.2Lab9Tests/task2_2Lab9Tests.swift
+++ task2.2Lab9Tests/task2_2Lab9Tests.swift
@@ -8,39 +8,43 @@
 
 import XCTest
 import CoreData
-import task2_2Lab9
+@testable import task2_2Lab9
 
 class task2_2Lab9Tests: XCTestCase {
 
    
-  lazy var  persistentContainer: NSPersistentContainer = {
-               /*
-                The persistent container for the application. This implementation
-                creates and returns a container, having loaded the store for the
-                application to it. This property is optional since there are legitimate
-                error conditions that could cause the creation of the store to fail.
-               */
-               let container = NSPersistentContainer(name: "task2_2Lab9")
-               container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-                   if let error = error as NSError? {
-                       // Replace this implementation with code to handle the error appropriately.
-                       // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                        
-                       /*
-                        Typical reasons for an error here include:
-                        * The parent directory does not exist, cannot be created, or disallows writing.
-                        * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                        * The device is out of space.
-                        * The store could not be migrated to the current model version.
-                        Check the error message to determine what the actual problem was.
-                        */
-                       fatalError("Unresolved error \(error), \(error.userInfo)")
-                   }
-               })
-               return container
-           }()
+    var  persistentContainer: NSPersistentContainer? = nil;
+    
     
     override func setUpWithError() throws {
+        
+        
+          persistentContainer = {
+            /*
+             The persistent container for the application. This implementation
+             creates and returns a container, having loaded the store for the
+             application to it. This property is optional since there are legitimate
+             error conditions that could cause the creation of the store to fail.
+            */
+            let container = NSPersistentContainer(name: "task2_2Lab9")
+            container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+                if let error = error as NSError? {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                     
+                    /*
+                     Typical reasons for an error here include:
+                     * The parent directory does not exist, cannot be created, or disallows writing.
+                     * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                     * The device is out of space.
+                     * The store could not be migrated to the current model version.
+                     Check the error message to determine what the actual problem was.
+                     */
+                    fatalError("Unresolved error \(error), \(error.userInfo)")
+                }
+            })
+            return container
+        }()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
        
@@ -69,7 +73,7 @@ class task2_2Lab9Tests: XCTestCase {
            searchRequest.predicate=NSPredicate(format: "from == %@ && to == %@", "Brest", "Minsk");
            
            
-        let foundRoutes=try persistentContainer.viewContext.fetch(searchRequest);
+        let foundRoutes=try persistentContainer!.viewContext.fetch(searchRequest);
         
         XCTAssertTrue(foundRoutes.count>0,"There should be some route from Brest to Misnk");
     }
@@ -81,7 +85,7 @@ class task2_2Lab9Tests: XCTestCase {
            searchRequest.predicate=NSPredicate(format: "from == %@ && to == %@", "Moscow", "Minsk");
            
            
-        let foundRoutes=try persistentContainer.viewContext.fetch(searchRequest);
+        let foundRoutes=try persistentContainer!.viewContext.fetch(searchRequest);
         
         XCTAssertTrue(foundRoutes.count>0,"There should be no route from Brest to Misnk");
     }
@@ -93,7 +97,7 @@ class task2_2Lab9Tests: XCTestCase {
             searchRequest.predicate=NSPredicate(format: "login == %@", "228");
            
            
-        let foundRoutes=try persistentContainer.viewContext.fetch(searchRequest);
+        let foundRoutes=try persistentContainer!.viewContext.fetch(searchRequest);
         
         XCTAssertTrue(foundRoutes.count == 0,"There should be no user ");
     }
@@ -104,7 +108,7 @@ class task2_2Lab9Tests: XCTestCase {
            searchRequest.predicate=NSPredicate(format: "login == %@", "admin");
            
            
-        let foundRoutes=try persistentContainer.viewContext.fetch(searchRequest);
+        let foundRoutes=try persistentContainer!.viewContext.fetch(searchRequest);
         
         XCTAssertTrue(foundRoutes.count>0,"There should be no user");
     }
